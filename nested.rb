35419f3bf1dbedfa5cdc514944b2eb2d0652d6d9
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-14 07:36:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,7 +78,7 @@ def adding_matz
           :languages => ["C"]
         },
         :yukihiro_matsumoto => {
-          :known_for +> "Ruby",
+          :known_for => "Ruby",
           :languages => ["LISP, C"]
         }
      }
