f9fa33db922037710b3579f437c8d98ba495a814
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-14 07:36:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -76,10 +76,12 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+        },
+        :yukihiro_matsumoto => {
+          :known_for +> "Ruby",
+          :languages => ["LISP, C"]
         }
      }
-
-    
 end
 
 def changing_alan
